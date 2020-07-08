b91d2c36d30778d67f86bfacc03e348232b3539c
Tiago Guedes <tiagopog@gmail.com>
2020-07-08 01:19:56 -0300

    Finish working on the main aspects of the snake mechanics

--- source/snake.py
+++ source/snake.py
@@ -28,19 +28,47 @@ class SnakeBodySegment:
         self.turning_point = None
         self.next = None
 
-    def grow(self):
-        self.next = SnakeBodySegment(
-            STARTING_X - 10,
-            STARTING_Y,
-            STARTING_DIRECTION_X,
-            STARTING_DIRECTION_Y,
-            STARTING_SPEED
+    @property
+    def is_going_up(self):
+        return (
+            self.direction_x == NEUTRAL_DIRECTION and
+            self.direction_y == POSITIVE_DIRECTION
         )
+
+    @property
+    def is_going_down(self):
+        return (
+            self.direction_x == NEUTRAL_DIRECTION and
+            self.direction_y == NEGATIVE_DIRECTION
+        )
+
+    @property
+    def is_going_left(self):
+        return (
+            self.direction_x == NEGATIVE_DIRECTION and
+            self.direction_y == NEUTRAL_DIRECTION
+        )
+
+    @property
+    def is_going_right(self):
+        return (
+            self.direction_x == POSITIVE_DIRECTION and
+            self.direction_y == NEUTRAL_DIRECTION
+        )
+
+    def draw(self):
+        arcade.create_rectangle_filled(
+            center_x=self.x,
+            center_y=self.y,
+            width=self.BODY_WIDTH,
+            height=self.BODY_HEIGHT,
+            color=arcade.color.WHITE
+        ).draw()
 
     def move(self):
         self.x += self.direction_x * self.speed
         self.y += self.direction_y * self.speed
-        self.check_turning_point()
+        self._check_turning_point()
 
     def turn_up(self):
         self.direction_x = NEUTRAL_DIRECTION
@@ -58,29 +86,45 @@ class SnakeBodySegment:
         self.direction_x = POSITIVE_DIRECTION
         self.direction_y = NEUTRAL_DIRECTION
 
-    def check_turning_point(self):
-        if self.turning_point is None:
-            return
-
-        x, y, direction = self.turning_point
-
-        if direction == 'up' and self.y >= y:
-            self.turn_up()
-            self.propagate_turning_point('up')
-            self.turning_point = None
+    def grow(self):
+        self.next = SnakeBodySegment(
+            STARTING_X - 10,
+            STARTING_Y,
+            STARTING_DIRECTION_X,
+            STARTING_DIRECTION_Y,
+            STARTING_SPEED
+        )
 
     def propagate_turning_point(self, direction):
         if self.next:
             self.next.turning_point = (self.x, self.y, direction)
 
-    def draw(self):
-        arcade.create_rectangle_filled(
-            center_x=self.x,
-            center_y=self.y,
-            width=self.BODY_WIDTH,
-            height=self.BODY_HEIGHT,
-            color=arcade.color.WHITE
-        ).draw()
+    def _check_turning_point(self):
+        if self.turning_point is None:
+            return
+
+        target_x, target_y, target_direction = self.turning_point
+        reached_x = self._has_reached_target_x
+        reached_y = self._has_reached_target_y
+
+        if reached_x(target_x, target_direction) or reached_y(target_y, target_direction):
+            getattr(self, f'turn_{target_direction}')()
+            self.propagate_turning_point(target_direction)
+            self.turning_point = None
+
+    def _has_reached_target_y(self, target_y, target_direction):
+        return (
+            target_direction in ('left', 'right') and
+            self.is_going_up and self.y >= target_y or
+            self.is_going_down and self.y <= target_y
+        )
+
+    def _has_reached_target_x(self, target_x, target_direction):
+        return (
+            target_direction in ('up', 'down') and
+            self.is_going_right and self.x >= target_x or
+            self.is_going_left and self.x <= target_x
+        )
 
 
 class Snake:
